c9bd86f1321c560a78c1e64f13c4433367b9ffdd
John-Ad <adriaanscorne@gmail.com>
2023-04-21 10:01:29 +0200

    made new convo page scrollable and updated suggestions height

--- lib/views/newConversation.dart
+++ lib/views/newConversation.dart
@@ -56,6 +56,9 @@ class _NewConversationState extends State<NewConversation> {
       "cold war",
     ];
 
+    var mainContentHeight =
+        MediaQuery.of(context).size.height - AppBar().preferredSize.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -75,166 +78,179 @@ class _NewConversationState extends State<NewConversation> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-              child: Text(
-                'New Conversation',
-                style: theme.textTheme.headlineSmall!.copyWith(
-                  color: theme.colorScheme.onPrimary,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.6,
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surface,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: Text(
-                      "Choose a topic",
-                      style: theme.textTheme.bodyLarge!.copyWith(
-                        color: theme.colorScheme.onPrimary,
-                      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: mainContentHeight,
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  child: Text(
+                    'New Conversation',
+                    style: theme.textTheme.headlineSmall!.copyWith(
+                      color: theme.colorScheme.onPrimary,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: Container(
-                      width: (MediaQuery.of(context).size.width * 0.8) * 0.8,
-                      height: (MediaQuery.of(context).size.height * 0.6) * 0.5,
-                      decoration: BoxDecoration(
-                        color: theme.colorScheme.primary,
-                        borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  // height: MediaQuery.of(context).size.height * 0.6,
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Text(
+                          "Choose a topic",
+                          style: theme.textTheme.bodyLarge!.copyWith(
+                            color: theme.colorScheme.onPrimary,
+                          ),
+                        ),
                       ),
-                      child: ListView(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                            child: Center(
-                              child: Text(
-                                "Suggestions:",
-                                style: theme.textTheme.bodyMedium!.copyWith(
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Container(
+                          width:
+                              (MediaQuery.of(context).size.width * 0.8) * 0.8,
+                          height:
+                              (MediaQuery.of(context).size.height * 0.6) * 0.6,
+                          decoration: BoxDecoration(
+                            color: theme.colorScheme.primary,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: ListView(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                child: Center(
+                                  child: Text(
+                                    "Suggestions:",
+                                    style: theme.textTheme.bodyMedium!.copyWith(
+                                      color: theme.colorScheme.onPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                                child: Container(
+                                  height: 1,
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(
+                                        color: theme.colorScheme.onPrimary,
+                                        width: 1,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              for (var topic in topics)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                  child: ListTile(
+                                    title: Center(
+                                      child: Text(
+                                        topic,
+                                        style:
+                                            theme.textTheme.bodySmall!.copyWith(
+                                          color: theme.colorScheme.onPrimary,
+                                        ),
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      _topicController.text = topic;
+                                    },
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Container(
+                          width:
+                              (MediaQuery.of(context).size.width * 0.8) * 0.8,
+                          height: 50,
+                          child: Theme(
+                            data: Theme.of(context).copyWith(
+                              textSelectionTheme: TextSelectionThemeData(
+                                selectionColor: theme.colorScheme.surface,
+                              ),
+                            ),
+                            child: TextField(
+                              controller: _topicController,
+                              cursorColor: theme.colorScheme.onPrimary,
+                              style: theme.textTheme.bodyLarge!.copyWith(
+                                color: theme.colorScheme.onPrimary,
+                              ),
+                              decoration: InputDecoration(
+                                fillColor: theme.colorScheme.primary,
+                                filled: true,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                                hintText: 'Enter a topic',
+                                hintStyle: theme.textTheme.bodyLarge!.copyWith(
+                                  color: theme.colorScheme.onPrimary
+                                      .withOpacity(0.3),
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.edit,
                                   color: theme.colorScheme.onPrimary,
                                 ),
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-                            child: Container(
-                              height: 1,
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                    color: theme.colorScheme.onPrimary,
-                                    width: 1,
-                                  ),
-                                ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                        child: SizedBox(
+                          width:
+                              (MediaQuery.of(context).size.width * 0.8) * 0.8,
+                          height: 50,
+                          child: ElevatedButton(
+                            // onPressed: _addConversation,
+                            onPressed: () {
+                              _addConversation();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: theme.colorScheme.tertiary,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              padding:
+                                  const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                            ),
+                            child: Text(
+                              'Continue',
+                              style: theme.textTheme.bodyLarge!.copyWith(
+                                color: theme.colorScheme.onPrimary,
                               ),
                             ),
                           ),
-                          for (var topic in topics)
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                              child: ListTile(
-                                title: Center(
-                                  child: Text(
-                                    topic,
-                                    style: theme.textTheme.bodySmall!.copyWith(
-                                      color: theme.colorScheme.onPrimary,
-                                    ),
-                                  ),
-                                ),
-                                onTap: () {
-                                  _topicController.text = topic;
-                                },
-                              ),
-                            ),
-                        ],
-                      ),
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: Container(
-                      width: (MediaQuery.of(context).size.width * 0.8) * 0.8,
-                      height: 50,
-                      child: Theme(
-                        data: Theme.of(context).copyWith(
-                          textSelectionTheme: TextSelectionThemeData(
-                            selectionColor: theme.colorScheme.surface,
-                          ),
-                        ),
-                        child: TextField(
-                          controller: _topicController,
-                          cursorColor: theme.colorScheme.onPrimary,
-                          style: theme.textTheme.bodyLarge!.copyWith(
-                            color: theme.colorScheme.onPrimary,
-                          ),
-                          decoration: InputDecoration(
-                            fillColor: theme.colorScheme.primary,
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: BorderSide.none,
-                            ),
-                            hintText: 'Enter a topic',
-                            hintStyle: theme.textTheme.bodyLarge!.copyWith(
-                              color:
-                                  theme.colorScheme.onPrimary.withOpacity(0.3),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.edit,
-                              color: theme.colorScheme.onPrimary,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: SizedBox(
-                      width: (MediaQuery.of(context).size.width * 0.8) * 0.8,
-                      height: 50,
-                      child: ElevatedButton(
-                        // onPressed: _addConversation,
-                        onPressed: () {
-                          _addConversation();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: theme.colorScheme.tertiary,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                        ),
-                        child: Text(
-                          'Continue',
-                          style: theme.textTheme.bodyLarge!.copyWith(
-                            color: theme.colorScheme.onPrimary,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
