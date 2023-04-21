5b3c885742f709bee55353b2f47261a38adf53b1
John-Ad <adriaanscorne@gmail.com>
2023-04-21 13:13:09 +0200

    fixed notification.metrics.axis == widget.axis: is not true issue

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -85,7 +85,7 @@ class _HomeState extends State<Home> with RouteAware {
           ),
           Expanded(
             child: ListView(
-              shrinkWrap: true,
+              shrinkWrap: false,
               children: [
                 for (var conversation in _conversations)
                   HomeConversation(
